2159563b3c3ad665da8f9e81e19619cb92347e3e
Kobus van Schoor <v.schoor.kobus@gmail.com>
2020-04-12 12:05:48 +0200

    made opt play rounds from both sides

--- tools/opt.py
+++ tools/opt.py
@@ -77,33 +77,50 @@ def merge(p1, p2):
     return norm_ind(n)
 
 def play_match(p1, p2):
-    with open(os.path.join(player_a, weights_file), 'w') as f:
-        json.dump(p1, f)
-    with open(os.path.join(player_b, weights_file), 'w') as f:
-        json.dump(p2, f)
+    seed = random.randint(0, 2 ** 16)
 
-    if os.path.isdir(logs_dir):
-        shutil.rmtree(logs_dir)
+    # set seed
+    with open(config_file, 'r') as f:
+        config = json.load(f)
+    config['seed'] = seed
+    with open(config_file, 'w') as f:
+        json.dump(config, f)
 
-    run('make run', starter_dir)
+    def play(lp, rp):
+        with open(os.path.join(player_a, weights_file), 'w') as f:
+            json.dump(lp, f)
+        with open(os.path.join(player_b, weights_file), 'w') as f:
+            json.dump(rp, f)
 
-    match_dir = os.path.join(logs_dir, list(os.listdir(logs_dir))[0])
-    rounds = [r for r in os.listdir(match_dir) if
-            os.path.isdir(os.path.join(match_dir, r))]
-    rounds = [int(r.lstrip('Round ')) for r in rounds]
-    last_round = f'Round {max(rounds)}'
+        if os.path.isdir(logs_dir):
+            shutil.rmtree(logs_dir)
 
-    endgame_file = os.path.join(match_dir, last_round, 'endGameState.txt')
-    winner = None
-    if os.path.isfile(endgame_file):
-        with open(endgame_file, 'r') as f:
-            for line in f.readlines():
-                if line.startswith('The winner is:'):
-                    winner = line[15]
-                    break
-    if winner is None:
-        return random.choice((p1, p2))
-    return p1 if winner == 'A' else p2
+        run('make run', starter_dir)
+
+        match_dir = os.path.join(logs_dir, list(os.listdir(logs_dir))[0])
+        rounds = [r for r in os.listdir(match_dir) if
+                os.path.isdir(os.path.join(match_dir, r))]
+        rounds = [int(r.lstrip('Round ')) for r in rounds]
+        last_round = f'Round {max(rounds)}'
+
+        endgame_file = os.path.join(match_dir, last_round, 'endGameState.txt')
+        winner = None
+        if os.path.isfile(endgame_file):
+            with open(endgame_file, 'r') as f:
+                for line in f.readlines():
+                    if line.startswith('The winner is:'):
+                        winner = line[15]
+                        break
+        if winner is None:
+            return None
+        elif winner == 'A':
+            return lp
+        else:
+            return rp
+
+    w1 = play(p1, p2)
+    w2 = play(p2, p1)
+    return w1 if w1 == w2 else None
 
 def next_round(pop, pbar):
     random.shuffle(pop)
@@ -114,18 +131,20 @@ def next_round(pop, pbar):
         p1, p2 = pair
         p1c, p2c = 0, 0
 
-        for _ in range(best_out_of):
-            if play_match(p1, p2) == p1:
-                p1c += 1
-            else:
-                p2c += 1
+        for _ in range(rounds):
+            w = play_match(p1, p2)
+            if w is not None:
+                if w == p1:
+                    p1c += 1
+                else:
+                    p2c += 1
             pbar.update()
 
         winners.append(p1 if p1c > p2c else p2)
 
     return winners
 
-best_out_of = 5
+rounds = 3
 pop_size = 2 ** 6 # 64
 generations = 50
 
@@ -140,7 +159,7 @@ print('starting training')
 
 runs = (pop_size / 2) * generations
 runs += pop_size - 1
-runs *= best_out_of
+runs *= rounds
 
 with tqdm(total=runs) as pbar:
     for gen in range(generations):
